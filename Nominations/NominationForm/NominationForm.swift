3d77dce2f6b7a280ff05ed8b0f0d9baaa818c81d
szuyun <musicabbage@gmail.com>
2023-10-28 23:09:02 +0100

    chore: Extract NominationForm's subviews to properties

--- Nominations/NominationForm/NominationForm.swift
+++ Nominations/NominationForm/NominationForm.swift
@@ -30,48 +30,18 @@ struct NominationForm<ViewModel: NominationViewModelProtocol>: View {
                         .aspectRatio(contentMode: .fill)
                         .frame(maxWidth: .infinity)
                     VStack(spacing: 34) {
-                        FormTitleView(title: .string("I’d like to nominate... "), description: "Please select a cube who you feel has done something honourable this month or just all round has a great work ethic.")
-                        FormFieldView(title: "Cube’s name", isRequired: true) {
-                            NomineesPickerView(nominees: $viewModel.nomineesList, selectedNomineeIndex: $viewModel.nomineeIndex)
+                        ForEach(Field.allCases) { field in
+                            titleView(field: field)
+                            controlView(field: field)
+                            if field.id < Field.allCases.count - 1 {
+                                Divider()
+                            }
                         }
-                        Divider()
-                        FormTitleView(title: .string("I’d like to nominate THIS CUBE BECAUSE..."), description: "Please let us know why you think this cube deserves the ‘cube of the month’ title 🏆⭐")
-                            .frame(maxWidth: .infinity)
-                        FormFieldView(title: "Reasoning", isRequired: true) {
-                            TextEditor(text: $viewModel.reason)
-                                .style(.body)
-                                .border(.cubeDarkGrey, width: 1)
-                                .frame(minHeight: 207)
-                        }
-                        Divider()
-                        FormTitleView(title: .attributedString(getProcessAttributedTitle()), description: "As you know, out the nominees chosen, we spin a wheel to pick the cube of the month. What’s your opinion on this method?")
-                        
-                        RadioButtonView(items: RadioButtonView.RadioItem.processOptions, selectedId: $viewModel.process)
-                            .padding(.bottom, 70)
                     }
-                    .padding(.horizontal, 16)
+                    .padding(EdgeInsets(top: 30, leading: 16, bottom: 70, trailing: 16))
                 }
                 
-                HStack(spacing: 14) {
-                    Spacer()
-                    CubeButton(state: .constant(.active),
-                               type: .secondary,
-                               title: "Back",
-                               action: { showConfirmView = true })
-                    CubeButton(state: $saveButtonState,
-                               type: .primary,
-                               title: "Next",
-                               action: sendNomination)
-                        .containerRelativeFrame(.horizontal, count: 5, span: 3, spacing: 0)
-                    Spacer()
-                }
-                .frame(maxHeight: 91.38)
-                .background(
-                    Rectangle()
-                        .fill(Color.white)
-                        .shadow(.strong)
-                        .ignoresSafeArea()
-                )
+                stickyButtonsView
             }
             if showConfirmView {
                 ConfirmLeaveView()
@@ -90,6 +60,115 @@ struct NominationForm<ViewModel: NominationViewModelProtocol>: View {
 }
 
 private extension NominationForm {
+    var stickyButtonsView: some View {
+        HStack(spacing: 14) {
+            Spacer()
+            CubeButton(state: .constant(.active),
+                       type: .secondary,
+                       title: "Back",
+                       action: { showConfirmView = true })
+            CubeButton(state: $saveButtonState,
+                       type: .primary,
+                       title: "Next",
+                       action: sendNomination)
+                .containerRelativeFrame(.horizontal, count: 5, span: 3, spacing: 0)
+            Spacer()
+        }
+        .frame(maxHeight: 91.38)
+        .background(
+            Rectangle()
+                .fill(Color.white)
+                .shadow(.strong)
+                .ignoresSafeArea()
+        )
+    }
+}
+
+private extension NominationForm {
+    
+    private enum Field: CaseIterable, Identifiable {
+        case nominee, reason, process
+        
+        typealias ID = Int
+        var id: Int {
+            switch self {
+            case .nominee: 0
+            case .reason: 1
+            case .process: 2
+            }
+        }
+        
+        var title: FormTitleView.TitleString {
+            switch self {
+            case .nominee:
+                return .string("I’d like to nominate... ")
+            case .reason:
+                return .string("I’d like to nominate THIS CUBE BECAUSE...")
+            case .process:
+                let textStyle = TextStyle.boldHeadlineSmall
+                let paragraphStyle = NSMutableParagraphStyle()
+                paragraphStyle.lineSpacing = textStyle.lineSpacing
+                let titleAttributes = AttributeContainer([
+                    .font: textStyle.font,
+                    .tracking: textStyle.letter,
+                    .paragraphStyle: paragraphStyle
+                ])
+                var title = AttributedString("IS HOW WE CURRENTLY RUN", attributes: titleAttributes)
+                title.foregroundColor = .black
+                var highlightText = AttributedString(" CUBE OF THE MONTH", attributes: titleAttributes)
+                highlightText.foregroundColor = .cubePink
+                title.append(highlightText)
+                title.append(AttributedString(" FAIR?"))
+                return .attributedString(title)
+            }
+        }
+        
+        var description: String {
+            switch self {
+            case .nominee:
+                "Please select a cube who you feel has done something honourable this month or just all round has a great work ethic."
+            case .reason:
+                "Please let us know why you think this cube deserves the ‘cube of the month’ title 🏆⭐"
+            case .process:
+                "As you know, out the nominees chosen, we spin a wheel to pick the cube of the month. What’s your opinion on this method?"
+            }
+        }
+        
+        var controlTitle: String {
+            switch self {
+            case .nominee: "Cube’s name"
+            case .reason: "Reasoning"
+            case .process: ""
+            }
+        }
+        
+        var required: Bool { true }
+    }
+    
+    private func titleView(field: Field) -> FormTitleView {
+        return FormTitleView(title: field.title, description: field.description)
+    }
+    
+    @ViewBuilder 
+    private func controlView(field: Field) -> some View {
+        switch field {
+        case .nominee:
+            FormFieldView(title: field.controlTitle, isRequired: field.required) {
+                NomineesPickerView(nominees: $viewModel.nomineesList, selectedNomineeIndex: $viewModel.nomineeIndex)
+            }
+        case .reason:
+            FormFieldView(title: field.controlTitle, isRequired: field.required) {
+                TextEditor(text: $viewModel.reason)
+                    .style(.body)
+                    .border(.cubeDarkGrey, width: 1)
+                    .frame(minHeight: 207)
+            }
+        case .process:
+            RadioButtonView(items: RadioButtonView.RadioItem.processOptions, 
+                            selectedId: $viewModel.process)
+        }
+    }
+    
     func sendNomination() {
         saveButtonState = .loading
         var success = false
