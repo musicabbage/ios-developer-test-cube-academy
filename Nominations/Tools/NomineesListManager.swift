65e13639da1f1e3c5ad5ca8c1eb824cc1c0bc87b
szuyun <musicabbage@gmail.com>
2023-10-29 22:02:31 +0000

    chore: Put read/write process in the same queue

--- Nominations/Tools/NomineesListManager.swift
+++ Nominations/Tools/NomineesListManager.swift
@@ -15,6 +15,7 @@ protocol NomineesListManagerProtocol {
 
 struct NomineesListManager: NomineesListManagerProtocol {
     private let localFilePath = URL.documentsDirectory.appending(path: ".nominees")
+    private let ioQueue = DispatchQueue(label: "com.nominations.nomineeslistmanager", qos: .default)
     private let networkService: NetworkService
     private let nomineeListSubject: CurrentValueSubject<NomineeListModel, Error> = .init(NomineeListModel(data: []))
     
@@ -44,27 +45,30 @@ private extension NomineesListManager {
     }
     
     func saveNomineeListToLocal(model: NomineeListModel) {
-        CubeLog(localFilePath.absoluteString)
-        do {
-            let encoder = JSONEncoder()
-            encoder.keyEncodingStrategy = .convertToSnakeCase
-            let data = try encoder.encode(model)
-            try data.write(to: localFilePath)
-        } catch {
-            CubeLog(error)
+        ioQueue.async {
+            do {
+                let encoder = JSONEncoder()
+                encoder.keyEncodingStrategy = .convertToSnakeCase
+                let data = try encoder.encode(model)
+                try data.write(to: localFilePath)
+            } catch {
+                CubeLog(error)
+            }
         }
     }
     
     func readLocalNomineeList() {
         guard FileManager.default.fileExists(atPath: localFilePath.path) else { return }
-        do {
-            let localData = try Data(contentsOf: localFilePath)
-            let decoder = JSONDecoder()
-            decoder.keyDecodingStrategy = .convertFromSnakeCase
-            let nomineeListModel = try decoder.decode(NomineeListModel.self, from: localData)
-            nomineeListSubject.send(nomineeListModel)
-        } catch {
-            CubeLog(error)
+        ioQueue.async {
+            do {
+                let localData = try Data(contentsOf: localFilePath)
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let nomineeListModel = try decoder.decode(NomineeListModel.self, from: localData)
+                nomineeListSubject.send(nomineeListModel)
+            } catch {
+                CubeLog(error)
+            }
         }
     }
 }
