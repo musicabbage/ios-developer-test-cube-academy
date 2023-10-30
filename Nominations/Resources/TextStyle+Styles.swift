964b900ed81ab293f37b68a1aaaa0ad59e804fc5
szuyun <musicabbage@gmail.com>
2023-10-30 17:44:18 +0000

    fix: Fix the `boldHeadlineSmallest` TextStyle for the form field's title.

--- Nominations/Resources/TextStyle+Styles.swift
+++ Nominations/Resources/TextStyle+Styles.swift
@@ -16,7 +16,7 @@ extension TextStyle {
     static let boldHeadlineLarge = TextStyle(.poppins, weight: 700, size: 32, lineHeight: 48, letter: 0.2)
     static let boldHeadlineMedium = TextStyle(.poppins, weight: 700, size: 24, lineHeight: 38, letter: 0.2)
     static let boldHeadlineSmall = TextStyle(.poppins, weight: 700, size: 18, lineHeight: 24, letter: 0.2)
-    static let boldHeadlineSmallest = TextStyle(.poppins, weight: 700, size: 18, lineHeight: 24, letter: 0.2)
+    static let boldHeadlineSmallest = TextStyle(.poppins, weight: 700, size: 16, lineHeight: 24, letter: 0.3)
 
     // MARK: Body
 
